5b40a46175bbb57048e78955789dcc36c36d738d
MbP04 <marcburpa@gmail.com>
2025-02-20 18:54:20 +0100

    update

--- bootstrap/proyecto/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.login__bloques__menu__toggle');
-    if (menuToggle) {
-      menuToggle.addEventListener('click', function () {
-        const menu = document.querySelector('.menu');
-        menu.classList.toggle('active');
-      });
-    }
-  });
